b91502ca3cd9c19980b4b62c4875f1e7a9c23408
quanht <zed.quan99@gmail.com>
2019-12-20 01:18:32 +0700

    check unauthen

--- src/api/api-sender.js
+++ src/api/api-sender.js
@@ -1,13 +1,20 @@
 import axios from "axios";
+import {notification} from "../utils/noti";
+import {logOut} from "../service/authen-service";
 const URL_BASE = process.env.REACT_APP_API_URL + "/api/s";
 
 function handleResult(res) {
-    return res.data;
+    if (res.data.code === 23) {
+        notification("error", "Phiên làm việc đã hết hạn. Xin vui lòng đăng nhập lại.");
+        logOut();
+        return res.data;
+    } else {
+        return res.data;
+    }
 }
 export function sendPostRequestWithoutToken(route, payload)
 {
     let route_api = `${URL_BASE}${route}`;
-    console.log(route_api)
     return axios.post(route_api, payload).then(handleResult);
 }
 export function sendGetRequest(route) {
@@ -15,7 +22,7 @@ export function sendGetRequest(route) {
     let token =  localStorage.getItem("token");
     let headers = {
         token:token
-    }
+    };
     return axios.get(route_api,{headers}).then(handleResult);
 }
 export function sendPutRequest(route, payload) {
@@ -23,6 +30,6 @@ export function sendPutRequest(route, payload) {
     let token =  localStorage.getItem("token");
     let headers = {
         token:token
-    }
+    };
     return axios.put(route_api,payload, {headers}).then(handleResult);
 }
